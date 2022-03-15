e56b2f610ce83790cf5c122750fc1dd4e72b3758
Aniro Montenegro <aniro.montenegro@anovasistemas.com.br>
2022-03-15 16:47:36 -0300

    Arquivo Conftest

---
+++ libpythonpro/tests/tests_spam/test_envio_para_usuarios.py
@@ -0,0 +1,12 @@
+from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.test_enviador_de_email import Enviador
+
+
+def test_envio_de_spam(sessao):
+    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+    enviador_de_spam.enviar_emails(
+        'capablanca1@bol.com.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+
+    )
